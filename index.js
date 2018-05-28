a1553d5050db8c27958c3db2427f75e5da40290f
wangjiling <vincentwangjiling@gmail.com>
2018-05-28 16:52:17 +0800

    init add

---
+++ index.js
@@ -0,0 +1,414 @@
+var ethereumjsUtil = require('ethereumjs-util');
+var hdkey = require('ethereumjs-wallet/hdkey');
+var EthereumTx = require('ethereumjs-tx');
+
+var Web3 = require('web3');
+var web3 = new Web3();
+
+var util = require('./util');
+
+var icap = require('ethereumjs-icap');
+
+var keythereum = require('keythereum');
+
+var ETHEREUM_MAINNET_PATH = "m/44'/60'/0'/0/0"; // ETH coin type is 60
+var ETHEREUM_TESTNET_PATH = "m/44'/1'/0'/0"; // Testnet (all coins) coin type is 1
+
+var bip39 = require('bip39');
+
+/**
+ * Mnemonic to seed
+ * @param {String} mnemonic
+ * @return {Buffer} seed
+ */
+function mnemonicToSeed(mnemonic) {
+	var seed = bip39.mnemonicToSeed(mnemonic);
+	return seed;
+}
+
+/**
+ * Seed to address
+ * @param {Buffer} seed
+ * @return {String} address
+ */
+function seedToAddress(seed) {
+	var hd = hdkey.fromMasterSeed(seed);
+	var wallet = hd.derivePath(ETHEREUM_MAINNET_PATH).getWallet();
+	return ethereumjsUtil.bufferToHex(wallet.getAddress());
+}
+
+/**
+ * Seed to checksum address
+ * @param {Buffer} seed
+ * @return {String} checksum address
+ */
+function seedToChecksumAddress(seed) {
+    var hd = hdkey.fromMasterSeed(seed);
+    var wallet = hd.derivePath(ETHEREUM_MAINNET_PATH).getWallet();
+    return wallet.getChecksumAddressString();
+}
+
+/**
+ * Hex-encoded seed to checksum address
+ * @param {String} seedHex: Hex-encoded seed
+ * @return {String} checksum address
+ */
+function seedHexToAddress(seedHex) {
+	var seed = Buffer.from(seedHex, 'hex');
+	return seedToChecksumAddress(seed);
+}
+
+/**
+ * Hex-encoded seed to [publicKey, address]
+ * @param {String} seedHex: Hex-encoded seed
+ * @return {Object} [String, String]
+ */
+function seedHexToPubAddr(seedHex) {
+    var seed = Buffer.from(seedHex, 'hex');
+    var hd = hdkey.fromMasterSeed(seed);
+    var wallet = hd.derivePath(ETHEREUM_MAINNET_PATH).getWallet();
+    var publicKey = wallet.getPublicKey().toString('hex');
+    var address = wallet.getChecksumAddressString();
+    return [publicKey, address];
+}
+
+/**
+ * Hex-encoded seed to privateKey
+ * @param {String} seedHex: Hex-encoded seed
+ * @return {Buffer} privateKey
+ */
+function seedHexToPrivate(seedHex) {
+    var seed = Buffer.from(seedHex, 'hex');
+    var hd = hdkey.fromMasterSeed(seed);
+    var wallet = hd.derivePath(ETHEREUM_MAINNET_PATH).getWallet();
+    return wallet.getPrivateKey();
+}
+
+/**
+ * verify address
+ * @param {String} address
+ * @return {Boolean}
+ */
+function isValidAddress(address) {
+	return ethereumjsUtil.isValidAddress(address)
+}
+
+/**
+ * verify checksum address
+ * @param {String} address
+ * @return {Boolean}
+ */
+function isValidChecksumAddress(address) {
+	return ethereumjsUtil.isValidChecksumAddress(address)
+}
+
+/**
+ * Checks if the given string is an address
+ *
+ * @method isAddress
+ * @param {String} address the given HEX adress
+ * @return {Boolean}
+ */
+function isAddress(address) {
+    return web3.isAddress(address)
+}
+
+/**
+ * iban to address
+ * @param {String} iban
+ * @return {String} address
+ */
+function ibanToAddress(iban) {
+    return ethereumjsUtil.toChecksumAddress(icap.toAddress(iban))
+}
+
+/**
+ * address to iban
+ * @param {String} address
+ * @return {String} iban
+ */
+function addressToIban(address) {
+    return icap.fromAddress(address, false, true)
+}
+
+/**
+ * build a eth transaction
+ * @param {Number|String} amountWei
+ * @param {String} addressTo
+ * @param {Number|String} nonce
+ * @param {Number|String} gasPrice
+ * @param {Number|String} gasLimit
+ * @param {String} customData
+ * @param {String} privateKey: Hex-encoded
+ * @return {Array} [txid, serializedTx]
+ */
+function buildEthTransaction(privateKey, amountWei, addressTo, nonce, gasPrice, gasLimit, customData) {
+    privateKey = Buffer.from(privateKey, 'hex');
+    var transaction = new EthereumTx({
+        nonce: web3.toHex(nonce),
+        gasPrice: web3.toHex(gasPrice),
+        gasLimit: web3.toHex(gasLimit),
+        to: addressTo,
+        value: web3.toHex(amountWei),
+        data: (customData && customData.length > 0) ? customData : '0x',
+        chainId: 1
+    });
+    transaction.sign(privateKey);
+    var txid = ('0x' + transaction.hash().toString('hex'));
+    var serializedTx = ('0x' + transaction.serialize().toString('hex'));
+
+    console.log('buildEthTransaction-transaction: ' + JSON.stringify(transaction));
+
+    return [txid, serializedTx];
+}
+
+/**
+ * build a eth transaction by Hex-encoded seed
+ * @param {String} seedHex: Hex-encoded seed
+ * @param {Number|String} amountWei
+ * @param {String} addressTo
+ * @param {Number|String} nonce
+ * @param {Number|String} gasPrice
+ * @param {Number|String} gasLimit
+ * @param {String} customData
+ * @return {Array} [txid, serializedTx]
+ */
+function buildEthTxBySeedHex(seedHex, amountWei, addressTo, nonce, gasPrice, gasLimit, customData) {
+    var privateKey = seedHexToPrivate(seedHex);
+    return buildEthTransaction(privateKey, amountWei, addressTo, nonce, gasPrice, gasLimit, customData)
+}
+
+/**
+ * build a token transaction
+ * @param {String} privateKey: Hex-encoded
+ * @param {Number|String} amountWei
+ * @param {String} addressTo
+ * @param {Number|String} nonce
+ * @param {Number|String} gasPrice
+ * @param {Number|String} gasLimit
+ * @param {String} contractAddress
+ * @return {Array} [txid, serializedTx]
+ */
+function buildTokenTransaction(amountWei, addressTo, nonce, contractAddress, gasLimit, gasPrice, privateKey) {
+    privateKey = Buffer.from(privateKey, 'hex');
+    var data = util.createTokenData(web3, amountWei, addressTo);
+    //  console.log('Data', data);
+    var raw = util.mapEthTransaction(web3, contractAddress, '0', nonce, gasPrice, gasLimit, data);
+    // console.log(raw);
+    var transaction = new EthereumTx(raw);
+    //console.log(transaction);
+    transaction.sign(privateKey);
+    var serializedTx = ('0x' + transaction.serialize().toString('hex'));
+    var txid = ('0x' + transaction.hash().toString('hex'));
+    return [txid, serializedTx];
+}
+
+/**
+ * build a token transaction by Hex-encoded seed
+ * @param {String} seedHex: Hex-encoded seed
+ * @param {Number|String} amountWei
+ * @param {String} addressTo
+ * @param {Number|String} nonce
+ * @param {Number|String} gasPrice
+ * @param {Number|String} gasLimit
+ * @param {String} contractAddress
+ * @return {Array} [txid, serializedTx]
+ */
+function buildTokenTxBySeedHex(seedHex, amountWei, addressTo, nonce, contractAddress, gasLimit, gasPrice) {
+    var privateKey = seedHexToPrivate(seedHex);
+    return buildTokenTransaction(amountWei, addressTo, nonce, contractAddress, gasLimit, gasPrice, privateKey)
+}
+
+/**
+ * build a eos map transaction
+ * @param {String} privateKey: Hex-encoded
+ * @param {String} eosPublicKey
+ * @param {Number|String} nonce
+ * @param {Number|String} gasPrice
+ * @param {Number|String} gasLimit
+ * @param {String} contractAddress
+ * @return {Array} [txid, serializedTx]
+ */
+function buildMapEosTransaction(eosPublicKey, nonce, contractAddress, gasLimit, gasPrice, privateKey) {
+    privateKey = Buffer.from(privateKey, 'hex');
+    var data = util.getTxData('register', ['string'], [eosPublicKey]);
+    //  console.log('Data', data);
+    var raw = util.mapEthTransaction(web3, contractAddress, '0', nonce, gasPrice, gasLimit, data);
+    // console.log(raw);
+    var transaction = new EthereumTx(raw);
+    //console.log(transaction);
+    transaction.sign(privateKey);
+    var serializedTx = ('0x' + transaction.serialize().toString('hex'));
+    var txid = ('0x' + transaction.hash().toString('hex'));
+    return [txid, serializedTx];
+}
+
+/**
+ * build a eos map transaction by Hex-encoded seed
+ * @param {String} seedHex: Hex-encoded seed
+ * @param {String} eosPublicKey
+ * @param {Number|String} nonce
+ * @param {Number|String} gasPrice
+ * @param {Number|String} gasLimit
+ * @param {String} contractAddress
+ * @return {Array} [txid, serializedTx]
+ */
+function buildMapEosTxBySeedHex(seedHex, eosPublicKey, nonce, contractAddress, gasLimit, gasPrice) {
+    var privateKey = seedHexToPrivate(seedHex);
+    return buildMapEosTransaction(eosPublicKey, nonce, contractAddress, gasLimit, gasPrice, privateKey)
+}
+
+/**
+ * Generate eos keyPair.
+ * @param {Function} cb is a Callback function, function params is {publicKey, privateKey}.
+ */
+function generateEosKeyPair(cb) {
+    util.generateEosKeyPair(cb);
+}
+
+/**
+ * Recover plaintext private key from secret-storage key object.
+ * @param {string} password.
+ * @param {string} keystoreContent: keystore file content.
+ * @return {Buffer} Plaintext private key.
+ */
+function getPrivateKeyFromKeystore (password, keystoreContent) {
+    var keyObject = JSON.parse(keystoreContent);
+    return keythereum.recover(password, keyObject);
+}
+
+/**
+ * Recover plaintext private key from secret-storage key object.
+ * @param {string} password.
+ * @param {string} keystoreContent: keystore file content.
+ * @return {String} Plaintext private key.
+ */
+function getHexPrivateKeyFromKeystore (password, keystoreContent) {
+    return getPrivateKeyFromKeystore (password, keystoreContent).toString('hex')
+}
+
+/**
+ * Returns the ethereum public key of a given private key
+ * @param {Buffer} privateKey A private key must be 256 bits wide
+ * @return {Buffer}
+ */
+function privateToPublic(privateKey) {
+    return ethereumjsUtil.privateToPublic(privateKey);
+}
+
+/**
+ * Returns the ethereum address of a given public key.
+ * Accepts "Ethereum public keys" and SEC1 encoded keys.
+ * @param {Buffer} pubKey The two points of an uncompressed key, unless sanitize is enabled
+ * @param {Boolean} [sanitize=false] Accept public keys in other formats
+ * @return {Buffer}
+ */
+function publicToAddress(pubKey, sanitize) {
+    return ethereumjsUtil.publicToAddress(pubKey, sanitize);
+}
+
+/**
+ * Returns the ethereum address of a given private key
+ * @param {Buffer} privateKey A private key must be 256 bits wide
+ * @return {Buffer}
+ */
+function privateToAddress(privateKey) {
+    return ethereumjsUtil.privateToAddress(privateKey);
+}
+
+/**
+ * Get privateKey&publicKey&address from secret-storage keystore file.
+ * @param {string} password.
+ * @param {string} keystoreContent: keystore file content.
+ * @return {Array} [privateKey, publicKey, address].
+ */
+function getKeyPairAddrFromKeystore (password, keystoreContent) {
+    var privateKey = getPrivateKeyFromKeystore(password, keystoreContent);
+    var publicKey = privateToPublic(privateKey);
+    var address = "0x" + privateToAddress(privateKey).toString('hex');
+    address = ethereumjsUtil.toChecksumAddress(address);
+
+    return [privateKey.toString('hex'), publicKey.toString('hex'), address]
+}
+
+/**
+ * Get privateKey&publicKey&address from secret-storage keystore file.
+ * @param {string} password.
+ * @param {string} keystoreContent: keystore file content.
+ * @param {Function} cb is a Callback function, function params is [privateKey, publicKey, address].
+ * @return {Array} [privateKey, publicKey, address].
+ */
+function getKeyPairAddrAsyncFromKeystore (password, keystoreContent, cb) {
+    var keyObject = JSON.parse(keystoreContent);
+    // Asynchronous
+    keythereum.recover(password, keyObject, function (privateKey) {
+        // do stuff
+        var publicKey = privateToPublic(privateKey);
+        var address = "0x" + privateToAddress(privateKey).toString('hex');
+        address = ethereumjsUtil.toChecksumAddress(address);
+
+        cb && cb([privateKey.toString('hex'), publicKey.toString('hex'), address])
+    });
+}
+
+/**
+ * Get publicKey&address from privateKey.
+ * @param {String|Buffer} privateKey ECDSA private key.
+ * @return {Array} [publicKey, address].
+ */
+function getPubAddrFromPrivate(privateKey) {
+    var privateKeyBuffer = keythereum.str2buf(privateKey);
+    if (privateKeyBuffer.length < 32) {
+        privateKeyBuffer = Buffer.concat([
+            Buffer.alloc(32 - privateKeyBuffer.length, 0),
+            privateKeyBuffer
+        ]);
+    }
+
+    var publicKey = privateToPublic(privateKeyBuffer);
+    var address = "0x" + privateToAddress(privateKeyBuffer).toString('hex');
+    address = ethereumjsUtil.toChecksumAddress(address);
+
+    return [publicKey.toString('hex'), address]
+}
+
+/**
+ * Returns a checksummed address
+ * @param {String} address
+ * @return {String}
+ */
+function toChecksumAddress(address) {
+    return ethereumjsUtil.toChecksumAddress(address);
+}
+
+
+module.exports = {
+    mnemonicToSeed: mnemonicToSeed,
+    seedToAddress: seedToAddress,
+    seedToChecksumAddress: seedToChecksumAddress,
+    seedHexToAddress: seedHexToAddress,
+    seedHexToPubAddr: seedHexToPubAddr,
+    seedHexToPrivate: seedHexToPrivate,
+    isValidAddress: isValidAddress,
+    isValidChecksumAddress: isValidChecksumAddress,
+    isAddress: isAddress,
+    buildEthTransaction: buildEthTransaction,
+    buildEthTxBySeedHex: buildEthTxBySeedHex,
+    buildTokenTransaction: buildTokenTransaction,
+    buildTokenTxBySeedHex: buildTokenTxBySeedHex,
+    buildMapEosTransaction: buildMapEosTransaction,
+    buildMapEosTxBySeedHex: buildMapEosTxBySeedHex,
+    generateEosKeyPair: generateEosKeyPair,
+    ibanToAddress: ibanToAddress,
+    addressToIban: addressToIban,
+    getPrivateKeyFromKeystore: getPrivateKeyFromKeystore,
+    getHexPrivateKeyFromKeystore: getHexPrivateKeyFromKeystore,
+    privateToPublic: privateToPublic,
+    publicToAddress: publicToAddress,
+    privateToAddress: privateToAddress,
+    getKeyPairAddrFromKeystore: getKeyPairAddrFromKeystore,
+    getKeyPairAddrAsyncFromKeystore: getKeyPairAddrAsyncFromKeystore,
+    getPubAddrFromPrivate: getPubAddrFromPrivate,
+    toChecksumAddress: toChecksumAddress
+};
+
---
+++ test/test.js
@@ -0,0 +1,183 @@
+/*
+*
+* simple tests
+*
+* */
+
+var expect = chai.expect;
+
+// timeout for asynchronous unit tests
+var TIMEOUT = 120000;
+
+var seedHex = '6fc2a047d00e5e9d883231023c92b8353085042915947d44a4ca239c9f1f7ab24cdb340dfc536430abb766f348e484bc776d120fd729292f0cdd39b2e8dc54a4'
+var mnemonic = 'favorite grape end strategy item horse first source popular cactus shine child'
+
+var seedHexToAddress = bridge.seedHexToAddress(seedHex)
+console.log('seedHexToAddress: ' + seedHexToAddress)
+console.log('seedHexToPubAddr: ' + JSON.stringify(bridge.seedHexToPubAddr(seedHex)))
+console.log('seedHexToPrivate: ' + bridge.seedHexToPrivate(seedHex).toString('hex'))
+
+var mnemonicToAddress = bridge.seedToAddress(bridge.mnemonicToSeed(mnemonic))
+console.log('mnemonicToSeed-seedToAddress: ' + mnemonicToAddress)
+
+console.log('isValidAddress: ' + bridge.isValidAddress(seedHexToAddress))
+console.log('isValidAddress: ' + bridge.isValidAddress('0x8617E340B3D01FA5F11F306F4090FD50E238070W'))
+console.log('isValidChecksumAddress: ' + bridge.isValidChecksumAddress(seedHexToAddress))
+console.log('isAddress: ' + bridge.isAddress('0x8617E340B3D01FA5F11F306F4090FD50E238070D'))
+console.log('isAddress: ' + bridge.isAddress('0xD1220A0cf47c7B9Be7A2E6BA89F429762e7b9aDb'))
+console.log('isAddress: ' + bridge.isAddress('0xd1220A0cf47c7B9Be7A2E6BA89F429762e7b9aDb'))
+
+console.log('ibanToAddress: ' + bridge.ibanToAddress('XE7338O073KYGTWWZN0F2WZ0R8PX5ZPPZS'))
+console.log('addressToIban: ' + bridge.addressToIban('0x00c5496aee77c1ba1f0854206a26dda82a81d6d8'))
+
+// get private key from keystore
+var keystoreContent = '{"version":3,"id":"ce14bddd-dc5b-4f24-b94c-1bae704f6866","address":"2a055947da8ba17ac751f2aa2ea5ecfee3db8c33","Crypto":{"ciphertext":"31743384b6bede7741d90445715e95600a108edb38fd118d65e07bbf2b1e2c68","cipherparams":{"iv":"8f7b036788323944affb3ddfe286a9d4"},"cipher":"aes-128-ctr","kdf":"scrypt","kdfparams":{"dklen":32,"salt":"d69affc8ef632ea27dedddacac00f714d17537c980193fb7dd26051d04eb4f57","n":8192,"r":8,"p":1},"mac":"581846f21e493383ae500012664d340c95dffb7897fb061519aedc9a9c96e915"}}'
+var privateKey = bridge.getPrivateKeyFromKeystore('123456789', keystoreContent)
+var hexPrivateKey = bridge.getHexPrivateKeyFromKeystore('123456789', keystoreContent)
+console.log('get private key from keystore: ' + privateKey.toString('hex'))
+console.log('get hex private key from keystore: ' + hexPrivateKey)
+
+var publicKey = bridge.privateToPublic(privateKey);
+console.log('private to public|length: ' + publicKey.toString('hex') + '|' + publicKey.length)
+
+console.log('public to address: ' + bridge.publicToAddress(publicKey).toString('hex'))
+console.log('private to address: ' + bridge.privateToAddress(privateKey).toString('hex'))
+
+var keyPairAddrArray = bridge.getKeyPairAddrFromKeystore('123456789', keystoreContent)
+console.log('getKeyPairAddrFromKeystore: ' + JSON.stringify(keyPairAddrArray))
+bridge.getKeyPairAddrAsyncFromKeystore('123456789', keystoreContent, function(arr) {
+    console.log('getKeyPairAddrAsyncFromKeystore: ' + JSON.stringify(arr))
+})
+
+console.log('getPubAddrFromPrivate: ' + JSON.stringify(bridge.getPubAddrFromPrivate(privateKey.toString('hex'))))
+
+
+// generate eos key pair
+bridge.generateEosKeyPair(function(eosKeyPair) {
+    console.log('eos key pair - ' + 'publicKey: ' + eosKeyPair.publicKey + ' privateKey: ' + eosKeyPair.privateKey)
+})
+
+// build tx
+console.log('buildEthTransaction:' + JSON.stringify(bridge.buildEthTransaction(privateKey.toString('hex'), 2441406250, '0xd46e8dd67c5d32be8058bb8eb970870f07244567', 0, 10e12, 30400)))
+
+
+describe('seed', function() {
+    it('seedHexToAddress', function() {
+        expect(seedHexToAddress).to.be.equal('0x9124bae940c2321DEd56f89B7e185b8785942303');
+    });
+
+    it('seedHexToPubAddr', function() {
+        expect(JSON.stringify(bridge.seedHexToPubAddr(seedHex))).to.be.equal('["e351cdae507b5c6f7d88e6966ff10d13be1668372cf1d2c60b26c851fbd41c3a6bf2452de42cdb6532aef93070760b32c0bb8f055ee258cc2973aab1d396aa53","0x9124bae940c2321DEd56f89B7e185b8785942303"]');
+    });
+
+    it('seedHexToPrivate', function() {
+        expect(bridge.seedHexToPrivate(seedHex).toString('hex')).to.be.equal('f21c74d3bf4464e1472343ce5bbd62a572afcf51e36d6b65ac003fe53c3dca3d');
+    });
+
+    it('seedToAddress', function() {
+        expect(bridge.seedToAddress(bridge.mnemonicToSeed(mnemonic))).to.be.equal('0x9124bae940c2321ded56f89b7e185b8785942303');
+    });
+});
+
+describe('mnemonic', function() {
+    it('mnemonicToSeed', function() {
+        expect(bridge.mnemonicToSeed(mnemonic).toString('hex')).to.be.equal(seedHex);
+    });
+});
+
+describe('verify address', function() {
+    it('isValidAddress true', function() {
+        expect(bridge.isValidAddress(seedHexToAddress)).to.be.equal(true);
+    });
+
+    it('isValidAddress false', function() {
+        expect(bridge.isValidAddress('0x8617E340B3D01FA5F11F306F4090FD50E238070W')).to.be.equal(false);
+    });
+
+    it('isValidChecksumAddress', function() {
+        expect(bridge.isValidChecksumAddress(seedHexToAddress)).to.be.equal(true);
+    });
+
+    it('isAddress true', function() {
+        expect(bridge.isAddress('0x8617E340B3D01FA5F11F306F4090FD50E238070D')).to.be.equal(true);
+        expect(bridge.isAddress('0xD1220A0cf47c7B9Be7A2E6BA89F429762e7b9aDb')).to.be.equal(true);
+    });
+
+    it('isAddress false', function() {
+        expect(bridge.isAddress('0xd1220A0cf47c7B9Be7A2E6BA89F429762e7b9aDb')).to.be.equal(false);
+    });
+});
+
+describe('iban', function() {
+    it('ibanToAddress', function() {
+        expect(bridge.ibanToAddress('XE7338O073KYGTWWZN0F2WZ0R8PX5ZPPZS')).to.be.equal('0x00c5496aEe77C1bA1f0854206A26DdA82a81D6D8');
+    });
+
+    it('addressToIban', function() {
+        expect(bridge.addressToIban('0x00c5496aee77c1ba1f0854206a26dda82a81d6d8')).to.be.equal('XE7338O073KYGTWWZN0F2WZ0R8PX5ZPPZS');
+    });
+});
+
+describe('import keystore', function() {
+    it('getPrivateKeyFromKeystore', function() {
+        expect(privateKey.toString('hex')).to.be.equal('ded772d10a77295b897824db5a4ab11c24a507a146cef2560460e491d242ccb9');
+    });
+
+    it('getHexPrivateKeyFromKeystore', function() {
+        expect(hexPrivateKey).to.be.equal('ded772d10a77295b897824db5a4ab11c24a507a146cef2560460e491d242ccb9');
+    });
+
+    // synchronous
+    it('getKeyPairAddrFromKeystore', function() {
+        expect(JSON.stringify(keyPairAddrArray)).to.be.equal('["ded772d10a77295b897824db5a4ab11c24a507a146cef2560460e491d242ccb9","18ad4ff97d0337b6434826daa2142137afac8cb39c28485414bb77289b26dd54de8a73efd888d904267d36fc42ebe6b10db2a337a5ceb3e7972aa532cd58a817","0x2a055947dA8bA17Ac751f2Aa2EA5EcfEe3Db8C33"]');
+    });
+    // asynchronous
+    it('getKeyPairAddrAsyncFromKeystore', function(done) {
+        this.timeout(TIMEOUT);
+        bridge.getKeyPairAddrAsyncFromKeystore('123456789', keystoreContent, function(arr) {
+            expect(JSON.stringify(arr)).to.be.equal('["ded772d10a77295b897824db5a4ab11c24a507a146cef2560460e491d242ccb9","18ad4ff97d0337b6434826daa2142137afac8cb39c28485414bb77289b26dd54de8a73efd888d904267d36fc42ebe6b10db2a337a5ceb3e7972aa532cd58a817","0x2a055947dA8bA17Ac751f2Aa2EA5EcfEe3Db8C33"]');
+            done();
+        })
+    });
+});
+
+describe('publicKey and privateKey', function() {
+    it('privateToPublic', function() {
+        expect(publicKey.toString('hex')).to.be.equal('18ad4ff97d0337b6434826daa2142137afac8cb39c28485414bb77289b26dd54de8a73efd888d904267d36fc42ebe6b10db2a337a5ceb3e7972aa532cd58a817');
+    });
+
+    it('privateToAddress', function() {
+        expect(bridge.privateToAddress(privateKey).toString('hex')).to.be.equal('2a055947da8ba17ac751f2aa2ea5ecfee3db8c33');
+    });
+
+    it('publicToAddress', function() {
+        expect(bridge.publicToAddress(publicKey).toString('hex')).to.be.equal('2a055947da8ba17ac751f2aa2ea5ecfee3db8c33');
+    });
+
+    it('getPubAddrFromPrivate', function() {
+        expect(JSON.stringify(bridge.getPubAddrFromPrivate(privateKey.toString('hex')))).to.be.equal('["18ad4ff97d0337b6434826daa2142137afac8cb39c28485414bb77289b26dd54de8a73efd888d904267d36fc42ebe6b10db2a337a5ceb3e7972aa532cd58a817","0x2a055947dA8bA17Ac751f2Aa2EA5EcfEe3Db8C33"]');
+    });
+});
+
+describe('checksum address', function() {
+    it('toChecksumAddress', function() {
+        expect(bridge.toChecksumAddress('0x9124bae940c2321ded56f89b7e185b8785942303')).to.be.equal('0x9124bae940c2321DEd56f89B7e185b8785942303');
+        expect(bridge.toChecksumAddress('9124bae940c2321ded56f89b7e185b8785942303')).to.be.equal('0x9124bae940c2321DEd56f89B7e185b8785942303');
+    });
+});
+
+describe('generate eos key pair', function() {
+    it('generateEosKeyPair', function(done) {
+        this.timeout(TIMEOUT);
+        bridge.generateEosKeyPair(function(eosKeyPair) {
+            expect(eosKeyPair).to.be.an('object');
+            done();
+        })
+    });
+});
+
+describe('build tx', function() {
+    it('buildEthTransaction', function() {
+        expect(JSON.stringify(bridge.buildEthTransaction(privateKey.toString('hex'), 2441406250, '0xd46e8dd67c5d32be8058bb8eb970870f07244567', 0, 10e12, 30400))).to.be.equal('["0xbf81b5ef67cfbe58e0fc7ec1a2df997c779d40aa351453cfa642a94f0602ae91","0xf869808609184e72a0008276c094d46e8dd67c5d32be8058bb8eb970870f07244567849184e72a8026a0f42aec174f05e0f4c71ed7c4e888f834692e5eec85d82a4c88755872ce3ab24da00b22378ad5e5fe2cc109522a7fdfcfbaeffbac6360048098418de712e125bd04"]');
+    });
+});
